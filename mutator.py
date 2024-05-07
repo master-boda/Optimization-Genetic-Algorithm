f6557cd8c7c55ba3356aaa4dcb1cee16ddcb6328
kikobatistaa <kiko.2205@hotmail.com>
2024-05-07 09:18:55 +0100

    faltava dar um import random ass batista

--- mutator.py
+++ mutator.py
@@ -1,3 +1,5 @@
+import random 
+
 def mutate(individual, mutation_rate=0.05):
     """ Aplica mutação aleatória em um indivíduo com uma dada taxa de mutação. """
     for i in range(len(individual)):
