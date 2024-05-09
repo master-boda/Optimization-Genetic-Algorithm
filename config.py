5fe4856b4d0f8ac3d532ea2aeb612ae6977cf831
master-boda <vicente.miranda2013@gmail.com>
2024-05-09 15:19:17 +0100

    config mais completo

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 class GAConfig:
     def __init__(self):
-        self.population_size = 400
+        self.population_size = 100
         self.num_generations = 50
         self.crossover_rate = 0.8
         self.mutation_rate = 0.05
+        self.elitism = True
         self.elitism_size = 2
+        self.selection = None
+        self.crossover = None
+        self.mutation = None
+        self.evaluator = None
 
+        
--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,15 +1,26 @@
 import random
 import numpy as np
-from config import GAConfig
-from utils import geo_matrix
-from utils import fitness_function
-from population import population
+from selection_algorithms import *
+from operators import *
+from population import *
+from utils import *
 
-def ga(initializer, evaluator, selection, crossover, mutation, og_matrix=False, maximize=True, verbose=True, elitism=True):
-    config = GAConfig()
-    
+def ga(initializer,
+    evaluator,
+    selection,
+    crossover,
+    mutation,
+    mutation_rate=0.05,
+    population_size=200,
+    num_generations=50,
+    crossover_rate=0.8,
+    elitism_size=2,
+    elitism=True,
+    og_matrix=False,
+    maximize=True,
+    verbose=True):    
     # initialize the population
-    population = initializer(config.population_size)
+    population = initializer(population_size)
     
     # select the Geo matrix to use (original=True uses the original matrix from the project instructions)
     matrix = geo_matrix(original=og_matrix)
@@ -18,58 +29,46 @@ def ga(initializer, evaluator, selection, crossover, mutation, og_matrix=False, 
     fitnesses = [evaluator(ind, matrix) for ind in population]
     
     if verbose:
-        print(f'Initial best fitness: {max(fitnesses) if maximize else min(fitnesses)}') 
-        print(f'Population size: {config.population_size}')
-        print(f'Number of generations: {config.num_generations}')
-        print(f'Geo matrix {"(original)" if og_matrix==True else ""}: {matrix.head(15)}')
+     print(f'Initial best fitness: {max(fitnesses) if maximize else min(fitnesses)}') 
+     print(f'Population size: {population_size}')
+     print(f'Number of generations: {num_generations}')
+     print(f'Geo matrix {"(original)" if og_matrix==True else ""}: {matrix.head(15)}')
     
-    for generation in range(config.num_generations):    
-        if elitism:
-            # select the individuals to be carried over to the next generation
-            sorted_indices = np.argsort(fitnesses)
-            sorted_indices = sorted_indices if maximize else sorted_indices[::-1]
-            
-            elite_indices = sorted_indices[-config.elitism_size:]
-            
-            offspring = [population[i] for i in elite_indices]
+    for generation in range(num_generations):    
+     if elitism:
+         # select the individuals to be carried over to the next generation
+         sorted_indices = np.argsort(fitnesses)
+         sorted_indices = sorted_indices if maximize else sorted_indices[::-1]
+         
+         elite_indices = sorted_indices[-elitism_size:]
+         
+         offspring = [population[i] for i in elite_indices]
 
-        else :
-            offspring = []
-            
-        while len(offspring) < config.population_size:
-            
-            p1 = selection(population, fitnesses)
-            p2 = selection(population, fitnesses)
-            
-            if random.random() < config.crossover_rate:
-                c1, c2 = crossover(p1, p2)
-            else:
-                c1, c2 = p1, p2
-                
-            #c1 = mutation(c1, config.mutation_rate)
-            #c2 = mutation(c2, config.mutation_rate)
-            
-            offspring.extend([c1, c2])
-            
-        population = offspring[:config.population_size]
-        fitnesses = [evaluator(ind, matrix) for ind in population]
-                
-        if verbose:
-            current_best_fitness = max(fitnesses) if maximize else min(fitnesses)
-            print(f'Generation {generation} best fitness: {current_best_fitness}')
-            
+     else :
+         offspring = []
+         
+     while len(offspring) < population_size:
+         
+         p1 = selection(population, fitnesses)
+         p2 = selection(population, fitnesses)
+         
+         if random.random() < crossover_rate:
+          c1, c2 = crossover(p1, p2)
+         else:
+          c1, c2 = p1, p2
+          
+         #c1 = mutation(c1, mutation_rate)
+         #c2 = mutation(c2, mutation_rate)
+         
+         offspring.extend([c1, c2])
+         
+     population = offspring[:population_size]
+     fitnesses = [evaluator(ind, matrix) for ind in population]
+          
+     if verbose:
+         current_best_fitness = max(fitnesses) if maximize else min(fitnesses)
+         print(f'Generation {generation} best fitness: {current_best_fitness}')
+         
     return population[np.argmin(fitnesses)], min(fitnesses)
-
-
-# expected parameters:
-# initializer:  population(number)
-# evaluator:    fitness_function(route, geo_matrix)
-# crossover:    crossover(p1, p2) -> c1, c2, ect.
-# mutation:     mutate(individual) -> individual
-# selection:    selection(population, fitnesses)
-
-from selection_algorithms import *
-from operators import partially_mapped_crossover
-from operators import simple_mutation
 
 ga(population, fitness_function, roulette_selection, partially_mapped_crossover, simple_mutation, False, True, True, True)
