1703d649873ccdbd2cd04ca1abdf5b8ef4c67022
Loris <lourencomouraomartins@gmail.com>
2024-05-13 23:49:53 +0100

    sys added

--- tests/func_tests.py
+++ tests/func_tests.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import random
 from pop.population import generate_individual
 from operators.crossovers import *
