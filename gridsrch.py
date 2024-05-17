4d34a1b482616527a5a884c271321064c46c5d61
Loris <lourencomouraomartins@gmail.com>
2024-05-17 22:55:55 +0100

    grrid

--- gridsrch.py
+++ gridsrch.py
@@ -3,10 +3,6 @@ import sys
 import os
 import itertools
 from multiprocessing import Pool
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-
 from main.genetic_algorithm import *
 from pop.population import *
 from operators.selection_algorithms import *
@@ -40,15 +36,17 @@ def perform_grid_search(param_grid, n_seeds=15):
     combos_with_seeds = [(seed, combo) for seed in seeds for combo in combinations]
 
     results = []
-    with tqdm(total=len(combos_with_seeds)) as pbar, Pool() as pool, open('grid_search_results.csv', 'w', newline='') as f:
+    with tqdm(total=len(combos_with_seeds)) as pbar, Pool() as pool:
         for i, combo in enumerate(combos_with_seeds):
             pbar.set_description(f"Running combination {i+1}/{len(combos_with_seeds)}")
             results.append(pool.apply_async(evaluate_combinations, (combo,)))
             pbar.update(1)
-
+        with tqdm(total=len(results), desc="Gathering results") as pbar:
+             for res in results:
+                results.append(res.get())
+                pbar.update(1)
         # Wait for all processes to finish and get the results
-        results = [result.get() for result in results]
-
+        
     # Aggregate results
     combination_scores = {}
     for (parameters, fitness) in results:
@@ -69,8 +67,8 @@ if __name__ == '__main__':
     param_grid = {
     'initializer': [population],
     'evaluator': [fitness_function],
-    'population_size': [50, 100, 200],
-    'num_generations': [50, 100, 200],
+    'population_size': [50, 100],
+    'num_generations': [50, 100],
     'mutation_rate': [0.05],
     'crossover_rate': [0.8],
     'elitism_size': [2, 5],
